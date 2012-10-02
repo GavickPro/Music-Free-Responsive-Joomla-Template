8779dc81b0b42685f9a9a80e926f20d004c70ee3
Tomasz Dziuda <dziudek@gmail.com>
2012-10-02 10:11:16 +0200

    Pagination fixed to show max. 7 pages - 3 before and 3 after the current page

--- gk_music_free/html/pagination.php
+++ gk_music_free/html/pagination.php
@@ -11,9 +11,34 @@ function pagination_list_render($list) {
 	$html .= '<li class="pagination-start">'.$list['start']['data'].'</li>';
 	$html .= '<li class="pagination-prev">'.$list['previous']['data'].'</li>';
 	
-		foreach($list['pages'] as $page) {
-			$html .= '<li>'.$page['data'].'</li>';
+	// search for the active page
+	$founded = 0;
+	
+	foreach($list['pages'] as $page) {
+		$founded++;
+		
+		if($page['active'] != 1) {
+			break;
 		}
+	}
+	// show only 7 pages - active, 3 before and 3 after
+	$iterator = 0;
+	
+	foreach($list['pages'] as $page) {
+		$iterator++;
+		
+		if($founded > 3 && $founded < 8) {
+			if($iterator >= $founded - 3 && $iterator <= $founded + 3) {
+				$html .= '<li>'.$page['data'].'</li>';
+			}
+		} else if($founded <= 3 || $founded >= 8) {
+			if($founded <= 3 && $iterator <= 7) {
+				$html .= '<li>'.$page['data'].'</li>';
+			} else if($founded >= 8 && $iterator > 3) {
+				$html .= '<li>'.$page['data'].'</li>';
+			}
+		}
+	}
 	
 	$html .= '<li class="pagination-next">'. $list['next']['data'].'</li>';
 	$html .= '<li class="pagination-end">'. $list['end']['data'].'</li>';
