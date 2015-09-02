1f53e756ed58edf10b6ffc7daab48b8148ec83e1
piotrkunicki <piotrkunicki@gavick.com>
2015-09-02 12:20:33 +0200

    Footer links changed

--- gk_music_free/layouts/blocks/footer.php
+++ gk_music_free/layouts/blocks/footer.php
@@ -3,13 +3,17 @@
 // No direct access.
 defined('_JEXEC') or die;
 
+$app    = JFactory::getApplication();
+$menu 	= $app->getMenu();
+$lang 	= JFactory::getLanguage();
+
 ?>
 
 <footer id="gkFooter">
 	<a href="#gkPageWrap" id="gkTopLink">Top</a>
 	
 	<?php if($this->API->get('framework_logo', '0') == '1') : ?>
-	<a href="//gavick.com" id="gkFrameworkLogo" title="Gavern Framework">Gavern Framework</a>
+	<a href="https://www.gavick.com/" id="gkFrameworkLogo" title="Gavern Framework">Gavern Framework</a>
 	<?php endif; ?>
 	
 	<?php if($this->API->modules('footer_nav')) : ?>
@@ -19,12 +23,20 @@ defined('_JEXEC') or die;
 	<?php if($this->API->get('copyrights', '') !== '') : ?>
 	<p>
 		<?php echo $this->API->get('copyrights', ''); ?> 
-		<a href="//www.gavick.com" title="Joomla Templates">Free Responsive Joomla! Template</a>
+		<?php if ($menu->getActive() == $menu->getDefault($lang->getTag())) : ?> 
+			<p>Joomla Template Design by <a href="https://www.gavick.com/" title="Joomla Templates">GavickPro</a></p>
+		<?php else : ?>
+			<p>Joomla Template Design by GavickPro</p>
+		<?php endif; ?>
 	</p>
 	<?php else : ?>
-	<p>
-		Template Design &copy; <a href="//www.gavick.com" title="Joomla Templates">Free Responsive Joomla! Template</a> GavickPro
-	</p>
+
+		<?php if ($menu->getActive() == $menu->getDefault($lang->getTag())) : ?> 
+			<p>Joomla Template Design by <a href="https://www.gavick.com/" title="Joomla Templates">GavickPro</a></p>
+		<?php else : ?>
+			<p>Joomla Template Design by GavickPro</p>
+		<?php endif; ?>
+	
 	<?php endif; ?>
 	
 	<?php if($this->API->get('stylearea', '0') == '1') : ?>
